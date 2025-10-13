57d7288e8460a80259414581e5a966bb8285027d
Ratha <phearom.ratha.2822@rupp.edu.kh>
2025-10-13 20:01:01 +0700

    Update connection.php

--- Server/connection.php
+++ Server/connection.php
@@ -1,21 +1,16 @@
 <?php
 $username = "root";
-<<<<<<< HEAD
+
 $psw="";
 $server ="localhost";
 $db="php-project";
 $con= mysqli_connect($server,$username,$psw,$db,3306);
-if(!$con){
 
-=======
-$psw="Rupp123$!";
-$server ="localhost";
-$db="php-project";
-$con= mysqli_connect($server,$username,$psw,$db,3307);
+
 if($con){
     echo "<aleart>connect success</aleart>";
 }else{
->>>>>>> ulvy/main
+
     echo"<aleart>connect success</aleart>";
 }
 
